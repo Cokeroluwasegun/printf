e2c9551b3bc6cef174c64b50bd0acd53aeeb875a
Coker Oluwasegun <cokeroluwasegun@outlook.com>
2021-10-03 23:48:59 -0700

    Created and pushed all file

---
+++ _printf.c
@@ -0,0 +1,53 @@
+#include "main.h"
+#include <stdarg.h>
+#include <stdio.h>
+/**
+* _printf - produce output
+* @format: String
+* Return: int
+*/
+int _printf(const char *format, ...)
+{
+int i = 0, j, n = 0, v = 0;
+va_list obj;
+typeToPrint arr[] = {{"c", printChar}, {"s", printString}, {"%", printPercent},
+{"i", printInteger}, {"d", printInteger}, {"b", printBinary},
+{"u", printUnsign}, {"o", printOctal}, {"x", printHexL},
+{"X", printHexU}, {"S", printString_S}, {"p", printPointer}, {NULL, NULL},};
+if (format == NULL)
+return (-1);
+va_start(obj, format);
+while (format && format[i])
+{
+j = 0;
+if (format[i] == '%')
+{
+while (arr[j].types)
+{
+if (*(arr[j].types) == format[i + 1])
+{
+n += arr[j].printType(obj);
+v = 1;
+}
+j++;
+}
+if (v == 0 && format[i + 1] != '\0')
+{
+_putchar(format[i]);
+_putchar(format[i + 1]);
+n += 2;
+}
+else if (v == 0 && format[i + 1] == '\0')
+return (-1);
+i++;
+}
+else
+{
+_putchar(format[i]);
+n++;
+}
+i++;
+}
+va_end(obj);
+return (n);
+}
---
+++ main.h
@@ -0,0 +1,43 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+
+/*===============================*/
+
+/*Dynamic function selection:*/
+
+/**
+* struct typeToPrint - the struct.
+* @types: the format.
+* @printType: the according function.
+* Return: void
+*/
+typedef struct typeToPrint
+{
+char *types;
+int (*printType)(va_list);
+} typeToPrint;
+
+/*===============================*/
+
+/*Printing functions:*/
+int printChar(va_list);
+int printString(va_list);
+int printPercent(va_list);
+int printInteger(va_list);
+int printBinary(va_list obj);
+int printUnsign(va_list obj);
+int printOctal(va_list obj);
+int printHexL(va_list obj);
+int printHexU(va_list obj);
+int printString_S(va_list obj);
+void print_hexa(unsigned int n);
+int printPointer(va_list obj);
+
+/*===============================*/
+
+/*Main function:*/
+int _printf(const char *format, ...);
+int _putchar(char c);
+
+#endif
